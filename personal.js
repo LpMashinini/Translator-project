9e539bf71375479b18b5b105fc3efbf5becf936c
Matshinini-Front-end-developer <lebogangp84@gmail.com>
2024-12-15 17:33:46 +0200

    modified fils

--- personal.js
+++ personal.js
@@ -1,7 +1,10 @@
 const tabLinks = document.querySelectorAll(".tabs");
 const tabContents = document.querySelectorAll(".tabs-content");
-const evelope = document.querySelector("fa-solid fa-envelope");
-// "fa-regular fa-envelope-open"
+const icon = document.querySelector(".fa-sun");
+const iconContainers = document.querySelectorAll(".container");
+const Icons = document.querySelectorAll("#icon");
+const circles = document.querySelectorAll(".circle");
+const iconSvg = document.querySelector(".iconSvg");
 
 
 const openTab = (tabName) => {
@@ -15,3 +18,33 @@ const openTab = (tabName) => {
     event.currentTarget.classList.add("active-tab")
     document.getElementById(tabName).classList.add("active-tab-content")
 }
+
+
+icon.onclick = () => {
+
+    icon.classList.toggle("fa-moon");
+
+    for (iconContainer of iconContainers) {
+
+        for(Icon of Icons){
+
+            for(circle of circles){
+
+                if (icon.classList.contains("fa-moon")) {
+                    iconContainer.style.background = "linear-gradient(90deg, rgba(14,30,174,1) 35%, rgba(8,79,191,1) 73%)";
+                    Icon.classList.add("active");
+                    circle.classList.add("active-circle");
+                } else {
+                    iconContainer.style.background = "black";
+                    Icon.classList.remove("active");
+                    circle.classList.remove("active-circle");
+                }
+
+            }
+            
+        }
+       
+    }
+};
+
+
