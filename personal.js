4c68edfaf8b8ec19d7767ffa997fc8ee92352fc5
Matshinini-Front-end-developer <lebogangp84@gmail.com>
2024-12-16 12:58:44 +0200

    added images

--- personal.js
+++ personal.js
@@ -5,16 +5,18 @@ const iconContainers = document.querySelectorAll(".container");
 const Icons = document.querySelectorAll("#icon");
 const circles = document.querySelectorAll(".circle");
 const iconSvg = document.querySelector(".iconSvg");
-
+const iconToggle = document.querySelector(".toggleContainer");
+const navLinks = document.querySelectorAll(".link");
 
 const openTab = (tabName) => {
 
-    for(tabLink of tabLinks){
+    for (tabLink of tabLinks) {
         tabLink.classList.remove("active-tab");
     }
-    for(tabContent of tabContents){
+    for (tabContent of tabContents) {
         tabContent.classList.remove("active-tab-content");
     }
+
     event.currentTarget.classList.add("active-tab")
     document.getElementById(tabName).classList.add("active-tab-content")
 }
@@ -26,25 +28,32 @@ icon.onclick = () => {
 
     for (iconContainer of iconContainers) {
 
-        for(Icon of Icons){
+        for (Icon of Icons) {
 
-            for(circle of circles){
+            for (circle of circles) {
 
-                if (icon.classList.contains("fa-moon")) {
-                    iconContainer.style.background = "linear-gradient(90deg, rgba(14,30,174,1) 35%, rgba(8,79,191,1) 73%)";
-                    Icon.classList.add("active");
-                    circle.classList.add("active-circle");
-                } else {
-                    iconContainer.style.background = "black";
-                    Icon.classList.remove("active");
-                    circle.classList.remove("active-circle");
+
+                for (navLink of navLinks) {
+
+                    if (icon.classList.contains("fa-moon")) {
+                        iconContainer.style.background = "linear-gradient(90deg, rgba(14,30,174,1) 35%, rgba(8,79,191,1) 73%)";
+                        Icon.classList.add("active");
+                        icon.classList.add("activeIcon");
+                        circle.classList.add("active-circle");
+                        iconToggle.classList.add("active");
+                    } else {
+                        iconContainer.style.background = "black";
+                        Icon.classList.remove("active");
+                        circle.classList.remove("active-circle");
+                        iconToggle.classList.remove("active");
+                        icon.classList.remove("activeIcon");
+                        iconToggle.classList.remove("active");
+                    }
                 }
 
             }
-            
+
         }
-       
+
     }
 };
-
-
